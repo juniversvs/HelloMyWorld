0c5b7ce351d765b316cda4db15faf614e302c7bc
junivers0711 <stivens.s.mendez.a@gmail.com>
2026-09-07 07:42:37 -0500

    version base

---
+++ script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper (".mySwiper", {
+
+
+     slidesPerView: 1,
+     spaceBetween: 30,
+     grabCursor:true,
+     loop:true,
+     breakpoints : {
+        991: {
+            slidesPerView:4
+        }
+     }
+
+});
